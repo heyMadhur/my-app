71f91606bc616c7eddca8be413bd127180710529
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-09 13:25:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ButtonUsage from './components/ButtonUsage';
+import Camera from './components/Camera/Camera';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KNOW Your Food</h1>
+      <ButtonUsage />
+      <Camera />
+    </div>
+  );
+}
+
+export default App;
